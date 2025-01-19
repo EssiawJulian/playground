694edd7c0812513b6d0ba0736927fcd6d77df529
julianessiaw <juliane@vt.edu>
2025-01-19 16:03:51 -0500

    Refactored code to utilize Express.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/** 
 const fs = require("fs");
 const path = require("path");
 const http = require("http");
@@ -6,11 +7,6 @@ const { error } = require("console");
 const homepage = http.createServer((req, res) => {
   switch (req.url) {
     case "/":
-      // fs.readFile(path.join(__dirname, 'index.html'), (err, data) => {
-      //   if (err) throw err;
-      //   res.writeHead(200, { "Content-Type": "text/html" });
-      //   res.end(data);
-      // });
       navPage("index.html", res);
       break;
     case "/about":
@@ -31,3 +27,32 @@ const navPage = (fileName, res) => {
 };
 
 homepage.listen(8000);
+*/
+
+// Now using Express.js
+const path = require("path");
+const express = require("express");
+const PORT = 3000;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname, "about.html"));
+});
+
+app.get("/contact", (req, res) => {
+  res.sendFile(path.join(__dirname, "contact-me.html"));
+});
+
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(__dirname, "404.html"));
+});
+
+app.listen(PORT, (err) => {
+  if (err) throw err;
+  console.log(`Running on Port:${PORT}`);
+});
